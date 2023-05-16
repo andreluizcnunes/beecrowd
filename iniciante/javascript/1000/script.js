dc902fcf308450ff827fcaefc434ab2491a5f1d3
Andre Nunes <andre.doctors@gmail.com>
2023-05-16 08:31:01 -0300

    resolution js 1000

---
+++ iniciante/javascript/1000/script.js
@@ -0,0 +1,5 @@
+// https://www.beecrowd.com.br/judge/pt/problems/view/1000
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log('Hello World!\n');
