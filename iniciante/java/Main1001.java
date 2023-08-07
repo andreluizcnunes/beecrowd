8f998b1ec5f956e99a4c656b1c5a2ae600f0c47b
André Nunes <andre.doctors@gmail.com>
2023-08-07 10:44:34 -0300

    resolution exercicio 1001

---
+++ iniciante/java/Main1001.java
@@ -0,0 +1,18 @@
+package iniciante.java;
+
+import java.util.Scanner;
+
+public class Main1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+        sc.close();
+    }
+}
