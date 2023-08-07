907821380d71f86480b9b24aa7a06dfc91b40312
André Nunes <andre.doctors@gmail.com>
2023-08-07 10:55:21 -0300

    deletando pasta javascript

--- iniciante/javascript/-modeloConfig/script.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-
-var a = Number(lines[0]);
-var b =  Number(lines[1]);
-
-var soma = a + b;
-
-console.log(soma + '\n');
--- iniciante/javascript/1000/script.js
+++
@@ -1,5 +0,0 @@
-// https://www.beecrowd.com.br/judge/pt/problems/view/1000
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-
-console.log('Hello World!\n');
