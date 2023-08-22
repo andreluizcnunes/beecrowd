3fe062654272faf6f6bb05c16863ec8efdf10923
André Nunes <andre.doctors@gmail.com>
2023-08-22 14:09:08 -0300

    exercice resolution 1007

---
+++ iniciante/java/Main1007.java
@@ -0,0 +1,22 @@
+package iniciante.java;
+
+import java.util.Scanner;
+
+public class Main1007 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            int A = sc.nextInt();            
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            int D = sc.nextInt();
+
+            int resultado = A * B - C * D;
+
+            System.out.println("DIFERENCA = " + resultado);
+
+
+        sc.close();
+    }
+}
